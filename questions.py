40d88a60c7db48c0b6705dd2b9fff72577c0c783
Ressia13 <119832276+Ressia13@users.noreply.github.com>
2025-03-18 20:16:46 -0300

    Update quiestions.py
    Agregada funcionalidad de puntuacion

--- questions.py
+++ questions.py
@@ -25,15 +25,14 @@ answers = [
 correct_answers_index = [1, 2, 0, 3, 1]
 
 # El usuario deberá contestar 3 preguntas
+score = 0 #inicializo la puntuacion
 for _ in range(3):
     # Se selecciona una pregunta aleatoria
     question_index = random.randint(0, len(questions) - 1)
-
     # Se muestra la pregunta y las respuestas posibles
     print(questions[question_index])
     for i, answer in enumerate(answers[question_index]):
         print(f"{i + 1}. {answer}")
-
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
         user_answer = (input("Respuesta: ")).strip()
@@ -46,12 +45,15 @@ for _ in range(3):
         # Se verifica si la respuesta es correcta
         if user_answer == correct_answers_index[question_index]:
             print("¡Correcto!")
+            score += 1
             break
     else:
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
+        score = score - 0.5
         print("Incorrecto. La respuesta correcta es:")
         print(answers[question_index][correct_answers_index[question_index]])
 
-    # Se imprime un blanco al final de la pregunta     ----- commit test 2
+    # Se imprime un blanco al final de la pregunta
     print()
+print("Su puntuacion final es ", str(score), "de 3")
