c601b17c301e1041489d6d0d813b6be74d722cfb
Tomás <119832276+Ressia13@users.noreply.github.com>
2025-03-19 19:13:25 -0300

    Add files via upload

---
+++ Entrega1/ActExtra.py
@@ -0,0 +1,89 @@
+'''El programa debe tener un menú interactivo que permita al usuario seleccionar las siguientes operaciones:
+- Agregar un nuevo producto al inventario, solicitando al usuario el nombre y la cantidad inicial del producto.
+- Eliminar un producto existente del inventario, solicitando al usuario el nombre del producto a eliminar.
+- Mostrar el inventario actual, que incluya el nombre y la cantidad de cada producto.
+- Salir del programa.
+
+El inventario puede ser almacenado utilizando un diccionario simple, donde las claves sean los nombres de los productos y los valores sean las cantidades.
+Se deben manejar situaciones simples como la introducción de productos duplicados o la eliminación de productos inexistentes.'''
+
+def show_menu():  #Se muestra el menú en consola
+    print("")
+    print("---- Menú ----")
+    print("1. Agregar producto al inventario")
+    print("2. Eliminar producto")
+    print("3. Mostrar inventario")
+    print("4. Salir")
+    print("------------")
+
+
+def add_prod(inv):  #Agrega un producto a la lista inv
+    product = [input("Nombre: "), (input("cantidad: ").strip())]
+    if product[1].isdigit():
+        product[1] = int(product[1])
+    else:
+        print("Cantidad inválida.") # si la cantidad no es un numero, cancela la operacion
+        return
+    for item in inv:
+        if item[0] == product[0] and product[0] != "":
+            print(f"---El producto {product[0]} ya está en el inventario---") #Verifico que el producto no este en el inventario
+            return
+    if product[1] > 0: #Si la cantidad es un numero natural, agrega el prodcuto
+        inv.append(product)
+        print()
+        print("---Producto agregado---")
+    else:
+        print()
+        print("Cantidad errónea, ingrese el producto nuevamente") #Sino, cancela la operacion
+
+
+def del_product(inv):
+    print()
+    if not inv:
+        print("Inventario vacío.") #Verifico que la lista no este vacia
+        return inv
+    nom = input("Ingrese el nombre del producto a eliminar: ")
+    new_inv = [i for i in inv if i[0] != nom] #Con comprension de listas, descarto los elementos en los que el nombre coincida
+    if len(new_inv) == len(inv):
+        print(f"No se encontró el producto {nom} en el inventario.") #Si las listas tienen el mismo tamaño, no eliminé nada
+    else:
+        print(f"Producto {nom} eliminado") #Sino, se eliminó un elemento
+    return new_inv   #Devuelve la lista nueva
+
+
+def show_inv(inv):
+    print()
+    if not inv:
+        print("##Inventario vacío##") #Verifico lista inv vacia
+        return
+    print("--- Inventario ---")
+    for producto in inv:   #imprimo todos los elementos de la lista
+        for elem in producto:
+            print(elem)
+        print()
+    print("------------")
+
+
+
+def main():
+    inv = []    #inicializo el inventario
+    while True:   #Muestra el menu en bucle
+        show_menu()
+        option= (input("Ingrese el numero de opción: "))
+        print("------------")
+        if option == "1":
+            add_prod(inv)
+        elif option == "2":
+            inv = del_product(inv)
+        elif option == "3":
+            show_inv(inv)
+        elif option == "4":
+            print("Cerrando el programa...")
+            break
+        else:
+            print("Numero de opcion invalido")
+            continue
+
+
+main()  #Ejecuto el modulo main
+
---
+++ Entrega1/questions.py
@@ -0,0 +1,60 @@
+import random
+
+# Preguntas para el juego
+questions = [
+    "¿Qué función se usa para obtener la longitud de una cadena en Python?",
+    "¿Cuál de las siguientes opciones es un número entero en Python?",
+    "¿Cómo se solicita entrada del usuario en Python?",
+    "¿Cuál de las siguientes expresiones es un comentario válido en Python?",
+    "¿Cuál es el operador de comparación para verificar si dos valores son iguales?",
+]
+# Respuestas posibles para cada pregunta, en el mismo orden que las preguntas
+answers = [
+    ("size()", "len()", "length()", "count()"),
+    ("3.14", "'42'", "10", "True"),
+    ("input()", "scan()", "read()", "ask()"),
+    (
+        "// Esto es un comentario",
+        "/* Esto es un comentario */",
+        "-- Esto es un comentario",
+        "# Esto es un comentario",
+    ),
+    ("=", "==", "!=", "==="),
+]
+# Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
+correct_answers_index = [1, 2, 0, 3, 1]
+
+# El usuario deberá contestar 3 preguntas
+questions_to_ask = random.sample(list(zip(questions, answers, correct_answers_index)), k=3)
+score = 0 #inicializo la puntuacion
+for questions, answers, correct_answers_index in questions_to_ask:
+    # Se selecciona una pregunta aleatoria
+    question_index = random.randint(0, len(questions) - 1)
+    # Se muestra la pregunta y las respuestas posibles
+    print(questions)
+    for i, answer in enumerate(answers):
+        print(f"{i + 1}. {answer}")
+    # El usuario tiene 2 intentos para responder correctamente
+    for intento in range(2):
+        user_answer = (input("Respuesta: ")).strip()
+        if user_answer.isdigit() and user_answer in {"1", "2", "3", "4"}: # verifico si el numero ingresado es válido
+            user_answer = int(user_answer) -1
+        else:
+            print("Respuesta no válida, cerrando el programa") # si no lo es, finalizo el programa con error
+            exit(1)
+
+        # Se verifica si la respuesta es correcta
+        if user_answer == correct_answers_index:
+            print("¡Correcto!")
+            score += 1
+            break
+    else:
+        # Si el usuario no responde correctamente después de 2 intentos,
+        # se muestra la respuesta correcta
+        score = score - 0.5
+        print("Incorrecto. La respuesta correcta es:")
+        print(answers[correct_answers_index])
+
+    # Se imprime un blanco al final de la pregunta
+    print()
+print("Su puntuacion final es ", str(score), "de 3")
