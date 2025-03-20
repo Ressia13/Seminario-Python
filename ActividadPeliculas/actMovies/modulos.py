d8736ceae11f27ef304c0695a43642c44497511e
Tomás <ressiagf@gmail.com>
2025-03-19 21:01:44 -0300

    Actividad de peliculas

--- ActividadPeliculas/actMovies/modulos.py
+++ ActividadPeliculas/actMovies/modulos.py
@@ -1,6 +1,7 @@
 
 
 def durac_prom(lista):
+    '''Este modulo recibe la lista de duraciones y si no es vacia, devuelve el promedio en float'''
     average = 0 if len(lista) == 0 else sum(lista) / len(lista)
     return average
 
