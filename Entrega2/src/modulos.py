f21d592140bf238e5beb163e4de7a6f374f789b0
Tomás <ressiagf@gmail.com>
2025-04-04 18:56:08 -0300

    Agregado ejercicio 10 y carpeta src

---
+++ Entrega2/src/modulos.py
@@ -0,0 +1,10 @@
+def calculate_score(kills, assists, deaths):
+    return (kills * 3) + (assists * 1) - (1 if deaths else 0)
+
+def print_ranking(score, final=False):
+    # Implementación de la función según el código proporcionado
+    pass
+
+def process_rounds(rounds):
+    # Implementación de la función según el código proporcionado
+    pass
